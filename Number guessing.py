0832973553eb444afd61f40d12184415fec7fb28
adreamerpy <kingsale1948@gmail.com>
2026-05-31 16:19:28 +0300

    Create Number guessing.py

---
+++ Number guessing.py
@@ -0,0 +1,16 @@
+--- Welcome to the Number Guessing Game! ---
+I am thinking of a number between 1 and 50. Can you guess it?
+Enter your guess: 65
+Too high! Try a lower number.
+Enter your guess: 46
+Too high! Try a lower number.
+Enter your guess: 44
+Too high! Try a lower number.
+Enter your guess: 36
+Too high! Try a lower number.
+Enter your guess: 26
+Too high! Try a lower number.
+Enter your guess: 24
+Wow! Congratulations! You guessed the number.
+
+[Program finished]
